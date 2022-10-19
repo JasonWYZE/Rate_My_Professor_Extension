c72ea28ce2f2cbdf4d8c32065a12ea3f83b25756
JasonWYZE <89949154+JasonWYZE@users.noreply.github.com>
2022-10-19 02:20:59 -0400

    update

--- scripts/content.js
+++ scripts/content.js
@@ -1,44 +1,4 @@
-// const article = document.querySelector("article");
 
-// // `document.querySelector` may return null if the selector doesn't match anything.
-// if (article) {
-//   const text = article.textContent;
-//   const wordMatchRegExp = /[^\s]+/g; // Regular expression
-//   const words = text.matchAll(wordMatchRegExp);
-//   // matchAll returns an iterator, convert to array to get word count
-//   const wordCount = [...words].length;
-//   const readingTime = Math.round(wordCount / 200);
-//   const badge = document.createElement("p");
-//   // Use the same styling as the publish information in an article's header
-//   badge.classList.add("color-secondary-text", "type--caption");
-//   badge.textContent = `⏱️ ${readingTime} min read`;
-
-//   // Support for API reference docs
-//   const heading = article.querySelector("h1");
-//   // Support for article docs with date
-//   const date = article.querySelector("time")?.parentNode;
-
-//   (date ?? heading).insertAdjacentElement("afterend", badge);
-// }
-
-
-
-// Course_listing = document.getElementsByClassName("WHLO WLKO")[0].getElementsByTagName("li")[0].getElementsByClassName("gwt-InlineLabel WG5F WF4F");
-
-
-// document.addEventListener("load", function(event) {
-//   console.log(document.getElementById('gwt-uid-4'))
-
-//   var courses = document.getElementsByClassName('gwt-InlineLabel WG5F WF4F');
-//   if(courses){
-//     console.log(courses.length);
-//     console.log(courses[0]);
-//     console.log(courses[0].textContent);
-//   }
-//   else{
-//     console.log("Null");
-//   }
-// });
 
 
 
@@ -57,27 +17,76 @@
 
 
 
+// var obs = new MutationObserver(function(event) 
+// {
+//   var courses = document.getElementsByClassName('gwt-InlineLabel WL5F WK4F');
+//   Array.from(courses).forEach(selector => 
+//     {
 
-// const selectors = document.getElementsByClassName('gwt-InlineLabel WL5F WK4F')
-// Array.from(selectors).forEach(selector => {
-//     document.arrive(selector, function(){
-//         console.log(selector.textContent)
+//         const professor = selector.textContent.substring(
+//             selector.textContent.lastIndexOf("Instructor:") + 11, 
+//             selector.textContent.lastIndexOf("Enrolled")-5).trim();
+
+//         console.log(professor)
 //     });
-// });
+// })
+
+// obs.observe(document.body, { childList: true, subtree: true, attributes: false, characterData: false });
 
 
-var obs = new MutationObserver(function(event) 
-{
-  var courses = document.getElementsByClassName('gwt-InlineLabel WL5F WK4F');
-  Array.from(courses).forEach(selector => 
-    {
 
-        const professor = selector.textContent.substring(
-            selector.textContent.lastIndexOf("Instructor:") + 11, 
-            selector.textContent.lastIndexOf("Enrolled")-5).trim();
+// professor_id = btoa("Teacher-2386143")
+// query = {
+//     query:"query RatingsListQuery($id: ID!) {node(id: $id) {... on Teacher {school {id} courseCodes {courseName courseCount} firstName lastName numRatings avgDifficulty avgRating department wouldTakeAgainPercent}}}",
+//     variables: { } 
+// }
+// query['variables']['id'] = professor_id
 
-        console.log(professor)
-    });
-})
+// fetch('https://www.ratemyprofessors.com/graphql', {
+//   method: 'POST', // or 'PUT'
+//   mode: 'cors',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     'Authorization' : 'Basic dGVzdDp0ZXN0',
+//     'Access-Control-Allow-Methdos': 'POST',
+//     'Referer' : 'https://www.ratemyprofessors.com/ShowRatings.jsp?tid=2386143' 
+//   },
+//   body: JSON.stringify(query),
+// }).then((data) => {
+//         console.log(data)
+//     });
 
-obs.observe(document.body, { childList: true, subtree: true, attributes: false, characterData: false });
+
+
+const xhr = new XMLHttpRequest()
+
+// listen for `load` event
+xhr.onload = () => {
+  // print JSON response
+  if (xhr.status >= 200 && xhr.status < 300) {
+    // parse JSON
+    const response = JSON.parse(xhr.responseText)
+    console.log(response)
+  }
+}
+
+// create a JSON object
+json = {
+    query:"query RatingsListQuery($id: ID!) {node(id: $id) {... on Teacher {school {id} courseCodes {courseName courseCount} firstName lastName numRatings avgDifficulty avgRating department wouldTakeAgainPercent}}}",
+    variables: { } 
+}
+professor_id = btoa("Teacher-2386143")
+json['variables']['id'] = professor_id
+
+// open request
+xhr.open('POST', 'https://www.ratemyprofessors.com/graphql')
+
+// set `Content-Type` header
+xhr.setRequestHeader('Content-Type', 'application/json')
+xhr.setRequestHeader('Authorization', 'Basic dGVzdDp0ZXN0')
+xhr.setRequestHeader('Referer', 'https://www.ratemyprofessors.com/ShowRatings.jsp?tid=2386143')
+
+
+// send rquest with JSON payload
+xhr.send(JSON.stringify(json))
+
