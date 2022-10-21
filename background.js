91c4e11a1586e443cf9cfabdae26664d5ed9abd4
JasonWYZE <jason.ze41@gmail.com>
2022-10-21 15:53:38 -0400

    test background.js

--- background.js
+++ background.js
@@ -1,18 +1,10 @@
-chrome.runtime.onMessage.addListener(function (url, sender, sendResponse) {
-    fetch(url).then((res) => {
-      res.json().then((json) => sendResponse(json));
-    });
 
+    chrome.runtime.onMessage.addListener(function (url, sender, sendResponse) {
+        fetch(url).then((res) => res.json().then((json) => sendResponse(json)));
+
+    // fetch(url).then(r => r.json())
+    // .then(json => sendResponse(json.Content));
+   
+    
     return true;
-
-//   console.log(
-//     sender.tab
-//       ? "from a content script:" + sender.tab.url
-//       : "from the extension"
-//   );
-//   if (request.greeting === "hello") sendResponse({ farewell: "goodbye" });
-//   else{
-//     sendResponse({ farewell: "something" })
-//   }
-
 });
